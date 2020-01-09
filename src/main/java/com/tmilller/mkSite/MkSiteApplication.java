65f64ada3861a26a8b663ded5842810f6114cadd
Theo Miller <theo.miller@ttiinc.com>
2020-01-09 10:05:15 -0600

    intial

---
+++ src/main/java/com/tmilller/mkSite/MkSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tmilller.mkSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmilller/mkSite/MkSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmilller.mkSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
